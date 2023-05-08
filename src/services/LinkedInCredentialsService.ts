6fcbe4f34f0a8aaf5112e173b4d5dea889cd70e1
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-05-08 18:07:24 +1000

    API to verify LinkedIn credentials

--- src/services/LinkedInCredentialsService.ts
+++ src/services/LinkedInCredentialsService.ts
@@ -2,23 +2,41 @@ import {apiUrl} from "../config/ApiConfiguration"
 import {handleResponse} from "../utils/HttpUtils"
 import {LinkedInCredentials} from "./models/LinkedInCredentials"
 
-interface LinkedInCredentialsInsertionRequest {
+interface LinkedInCredentialsRequest {
     readonly email: string
     readonly password: string
 }
 
 export const insertLinkedInCredentials =
-    async (linkedInCredentialsInsertionRequest: LinkedInCredentialsInsertionRequest): Promise<LinkedInCredentials> => {
+    async (linkedInCredentialsRequest: LinkedInCredentialsRequest): Promise<LinkedInCredentials> => {
         const response = await fetch(`${apiUrl}/linkedIn/credentials`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             credentials: "include",
-            body: JSON.stringify(linkedInCredentialsInsertionRequest)
+            body: JSON.stringify(linkedInCredentialsRequest)
         })
 
         return handleResponse(response)
+    }
+
+export const verifyLinkedInCredentials =
+    async (linkedInCredentialsRequest: LinkedInCredentialsRequest): Promise<void> => {
+        const response = await fetch(`${apiUrl}/linkedIn/credentials/verify`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(linkedInCredentialsRequest)
+        })
+
+        if (response.status === 200) {
+            return Promise.resolve()
+        } else {
+            return Promise.reject("Invalid LinkedIn credentials")
+        }
     }
 
 export const fetchLinkedInCredentials =
